f119cf90f5d88aa784678c2099c3474eef686ca9
Abdul-Muhaiman <abdulmuhaiman21@gmail.com>
2025-03-29 16:46:43 +0500

    DB setup correctly in the master branch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+                port: '', // Leave empty if no specific port is required
+                pathname: '/img/**', // Matches all paths under /img/
+            },
+        ],
+    },
+}
 
 export default nextConfig;
